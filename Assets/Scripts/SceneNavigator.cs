5d79133af502db1a2a8af83e5f19b553c4c4503c
Sparsh Chaudhary <work.sparsh01@gmail.com>
2025-02-10 06:35:37 +0100

    AnimojiScene
    Installed the Animoji Scene that uses blendshapes from iOS ARKit to map a 3D face to the user's face

---
+++ Assets/Scripts/SceneNavigator.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneNavigator : MonoBehaviour
+{
+    public void LoadFaceTrackingScene()
+    {
+        SceneManager.LoadSceneAsync("FaceTracking");
+    }
+}
